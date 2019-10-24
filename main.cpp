f0620093e1720d0f343cc12773195d617804009e
noimin0610 <mii.informationsystems@gmail.com>
2019-10-24 15:15:28 +0900

    4625945259 0出力するだけ

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll =  long long;
+using Pll = pair<ll, ll>;
+using Pii = pair<int, int>;
+
+constexpr int N = 100;
+constexpr int A_MAX = 100000;
+
+int a[N][N] = {};
+
+inline void input() {
+    for(int i=0;i<N;++i) for(int j=0;j<N;++j) cin >> a[i][j];
+}
+
+inline void solve() {
+
+}
+
+inline void output() {
+    cout << 0 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    input();
+    solve();
+    output();
+}
